ce32d46bb665e6b75a11a09876f0a07a16262890
Sam Gabrail <sam.gabrail@tekanaid.com>
2023-03-20 18:11:44 -0400

    working example

--- setup_crossplane.sh
+++ setup_crossplane.sh
@@ -11,21 +11,6 @@ kubectl get pods -n crossplane-system
 # Look at the new API end-points with kubectl api-resources
 kubectl api-resources  | grep crossplane
 
-# Install the AWS provider 
-
-cat <<EOF | kubectl apply -f -
-apiVersion: pkg.crossplane.io/v1
-kind: Provider
-metadata:
-  name: upbound-provider-aws
-spec:
-  package: xpkg.upbound.io/upbound/provider-aws:v0.27.0
-EOF
-
-# It may take up to five minutes for the provider to list HEALTHY as True.
-
-kubectl get providers
-
 # Create a Kubernetes secret for AWS 
 # Create a text file containing the AWS account aws_access_key_id and aws_secret_access_key.
 
@@ -36,44 +21,3 @@ generic aws-secret \
 
 # Check the secret
 kubectl describe secret
-
-# Create a ProviderConfig 
-# A ProviderConfig customizes the settings of the AWS Provider.
-
-cat <<EOF | kubectl apply -f -
-apiVersion: aws.upbound.io/v1beta1
-kind: ProviderConfig
-metadata:
-  name: default
-spec:
-  credentials:
-    source: Secret
-    secretRef:
-      namespace: crossplane-system
-      name: aws-secret
-      key: creds
-EOF
-
-# Create an S3 managed resource 
-
-cat <<EOF | kubectl create -f -
-apiVersion: s3.aws.upbound.io/v1beta1
-kind: Bucket
-metadata:
-  name: tekanaid-crossplane
-  labels:
-    docs.crossplane.io/example: provider-aws
-spec:
-  forProvider:
-    region: us-east-1
-  providerConfigRef:
-    name: default
-EOF
-
-
-kubectl get buckets
-
-# Delete the managed resource 
-
-kubectl delete bucket --selector=docs.crossplane.io/example=provider-aws
-
